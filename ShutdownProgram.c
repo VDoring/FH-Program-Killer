16b698006c835b6889f74db878a3894292b83327
Doring <hojun.dev@gmail.com>
2020-02-13 13:41:53 +0900

    Ver 2, make CUI and Function
    창 크기 등의 그래픽 요소 수정.
    프로세스 검색기능과 종료기능 제작.

--- ShutdownProgram.c
+++ ShutdownProgram.c
@@ -9,33 +9,39 @@
 #define KEY_RIGHT 4
 #define KEY_ENTER 5
 
-gotoxy(int x, int y) { //글자위치 조정 함수
+int Menu(); //메인메뉴
+int keyControl(); //메뉴에서 입력한 키보드키 반환
+void Menu_1(); //1.Run
+void Menu_2(); //2.Search
+
+//글자위치 조정 함수
+gotoxy(int x, int y) {
 	COORD pos = { x, y };
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
 }
 
-textcolor(int color_num) { //글자색 설정 함수
+//글자색 설정 함수
+textcolor(int color_num) {
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color_num);
 }
 
-
 //메인메뉴
 int Menu() {
-	int x = 47, y = 4; //gotoxy용 변수
+	int x = 10, y = 4; //gotoxy용 변수
 	int keynum; //키보드 키값 저장
 	int menu = 1; //몇번 메뉴를 선택했는지 알기위한 변수
 
-	system("mode con cols=128 lines=24");
-	system("title Program Shutdown v0.1");
+	system("mode con cols=34 lines=10");
+	system("title Process Killer v1.0");
 
-	gotoxy(50, 1); textcolor(11); printf("Process Killer");
-	gotoxy(x - 2, y); textcolor(15); printf("> 1.Search / Run");
+	gotoxy(10, 1); textcolor(11); printf("Process Killer");
+	gotoxy(x - 2, y); textcolor(15); printf("> 1.Run");
 	gotoxy(x, y + 1); textcolor(15); printf("2.Search");
 
 	while (1) {
 		keynum = keyControl();
 
-		switch (keynum) {
+		switch (keynum) { //키 누름에 따라 표시되는 화살표가 바뀜
 		case KEY_UP:
 			if (y > 4) {
 				gotoxy(x - 2, y); printf(" ");
@@ -69,7 +75,7 @@ int Menu() {
 int keyControl() {
 	char keytemp;
 
-	keytemp = getch();
+	keytemp = _getch();
 	if (keytemp == 13)
 		return KEY_ENTER;
 	switch (keytemp) {
@@ -86,28 +92,63 @@ int keyControl() {
 	}
 }
 
+// 1.Run
 void Menu_1() {
-	gotoxy(10, 10); printf("1111111111");
-	return 0;
+	const char taskkill_command[16] = { "taskkill /F /IM" };
+	char process_name[100] = { 0, }; //유저가 입력
+	char final_taskkill_command[120] = { 0, }; //최종 명령어
+
+	system("mode con cols=50 lines=10");
+	
+	gotoxy(2, 2); printf("Please enter the name of the program to exit");
+	gotoxy(7, 3); printf("(Please put an '.exe' on the end)");
+	gotoxy(3, 5); printf("->");
+	gotoxy(6, 5); scanf_s("%s", process_name, sizeof(process_name));
+
+	sprintf_s(final_taskkill_command, sizeof(final_taskkill_command), "%s %s", taskkill_command, process_name);
+	system(final_taskkill_command);
+
+	system("cls");
+	gotoxy(20, 2); textcolor(10); printf("Executed!"); Sleep(800);
+
+	return;
 }
 
+// 2.Search
 void Menu_2() {
-	gotoxy(10, 10); printf("2222222222");
-	return 0;
+	const char tasklist_imagename_command_start[28] = { "tasklist /FI \"IMAGENAME eq "};
+	const char tasklist_imagename_command_end[2] = { "\"" };
+	char process_name[100] = { 0, }; //유저가 입력
+	char final_tasklist_command[140] = { 0, }; //최종 명령어
+
+	system("mode con cols=50 lines=10");
+	gotoxy(2, 2); printf("Please enter the name of the program to search");
+	gotoxy(7, 3); printf("(Please put an '.exe' on the end)");
+	gotoxy(3, 5); printf("->");
+	gotoxy(6, 5); scanf_s("%s", process_name, sizeof(process_name));
+
+	sprintf_s(final_tasklist_command, sizeof(final_tasklist_command), "%s%s%s", tasklist_imagename_command_start, process_name, tasklist_imagename_command_end);
+	system("mode con cols=100 lines=35");
+	system(final_tasklist_command);
+
+	system("pause");
+
+	return;
 }
 
 int main() {
 	int menu_select_num = 0;
-
-	menu_select_num = Menu();
-	switch (menu_select_num) {
-	case 1:
-		Menu_1();
-		break;
-	case 2:
-		Menu_2();
-		break;
-	default:
-		return -1; //에러
+	while (1) {
+		menu_select_num = Menu();
+		switch (menu_select_num) {
+		case 1:
+			Menu_1();
+			break;
+		case 2:
+			Menu_2();
+			break;
+		default:
+			return -1; //에러
+		}
 	}
 }
